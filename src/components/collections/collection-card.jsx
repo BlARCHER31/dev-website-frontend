f7b2d43ece30924e2038b4ae5487a336c7d82abb
Blake Archer <blarcher31@gmail.com>
2022-03-07 20:56:38 -0600

    added alt description for category images

--- src/components/collections/collection-card.jsx
+++ src/components/collections/collection-card.jsx
@@ -5,7 +5,7 @@ import Banner from './../banner/banner'
 const CollectionCard = ({ image, banner }) => {
   return (
     <div className='collection-card'>
-      <img className='collection-image' src={image} />
+      <img className='collection-image' src={image} alt='Category' />
       <Banner title={banner} />
     </div>
   )
