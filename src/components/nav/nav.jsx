41cede68a04aa1dd8aca812b2778bc0afc286e87
Blake Archer <blarcher31@gmail.com>
2022-03-14 10:58:00 -0500

    addition of side nav bar

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AiOutlineMenu } from 'react-icons/ai'
+import { Link } from 'react-router-dom'
 import './nav.css'
 
 const NavBar = () => {
+  const [isActive, setActive] = useState(false)
+
+  const toggleClass = () => {
+    setActive(!isActive)
+  }
   return (
     <div className='navbar'>
       <p className='nav-title'> RiH</p>
-      <div className='burger'>
+      <div onClick={toggleClass} className='burger'>
         <AiOutlineMenu />
+      </div>
+      <div className={isActive ? 'sidebar sidebar-active' : 'sidebar'}>
+        <Link className='nav-link' to=''>
+          Products
+        </Link>
+        <Link className='nav-link' to=''>
+          About
+        </Link>
+        <Link className='nav-link' to=''>
+          Cart
+        </Link>
+        <Link className='nav-link' to=''>
+          Contact
+        </Link>
       </div>
     </div>
   )
